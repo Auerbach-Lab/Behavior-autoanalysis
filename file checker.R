fe80cbeda9250964592ea6a7a124ab9533481bef
Noelle James <github@nohelix.com>
2022-06-18 12:17:40 -0500

    Moved from breaking up called-for file name to compare against actual file to building an 'expected' name and comparing that to the called-for file.
    Given the variety of name types its hard to produce a reliable name digester. However, know what type of file it is lets you easily build the expected name.

--- file checker.R
+++ file checker.R
@@ -3,18 +3,6 @@
 # Assumes you have booth the 'called_for_file' and actual 'file_name'
 
 
-# Break down file name ----------------------------------------------------
-
-# Get 1st block of text which should be the go frequency (single or range)
-file_name_frequency = gsub(pattern = "(^.*?)_.*$", replacement = "\\1", file_name)
-
-# Get 1st block of text with dB. Note for octave files there are 2 of the blocks.
-file_name_intensity = gsub(pattern = "^.*_(.*dB)_.*$", replacement = "\\1", file_name)
-
-# Get 1st block of text with ms. Note for some files with reduced response or sensitivity windows, there are 2-3 blocks.
-file_name_duration = gsub(pattern = "^.*?_(.*?ms)_.*$", replacement = "\\1", file_name) # The ? makes it 'lazy' so only the 1st block is pulled
-
-
 # Actual Stim -------------------------------------------------------------
 
 # List of go sound frequencies
@@ -38,6 +26,11 @@ file_intensity_range_steps = file_frequency_ranges %>%
 file_duration = unique(stim_master_list["Dur (ms)"])
 
 file_response_window = unique(stim_master_list["Nose Out TL (s)"]) %>% as.numeric()
+
+
+# Build 'real' file name --------------------------------------------------
+
+
 
 # Check File name vs. file ------------------------------------------------
 
