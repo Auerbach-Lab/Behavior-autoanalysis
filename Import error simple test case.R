5028f20ce8477b4361b8eea3392ae44cbc109b71
Noelle James <github@nohelix.com>
2022-06-21 11:36:18 -0500

    R.matlab bug report. Simplest test case imports as list.

---
+++ Import error simple test case.R
@@ -0,0 +1,27 @@
+
+# Package loading ---------------------------------------------------------
+
+# data loading/manipulation
+library(R.matlab)
+
+# Running environment -----------------------------------------------------
+
+# from print(sessionInfo())
+# R version 4.1.2 (2021-11-01)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19044)
+#
+# Matrix products: default
+#
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base
+#
+# other attached packages:
+#   [1] R.matlab_3.6.2
+
+
+# Load file ---------------------------------------------------------------
+file = readMat("~/GitHub/Behavior-autoanalysis/Only_2by2table.mat")
+
+class(file$source.list)
+View(file$source.list)
