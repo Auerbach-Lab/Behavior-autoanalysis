6938cc7e7aae98e17cae60ff6c4e82c3cfbd9d11
Noelle James <github@nohelix.com>
2022-06-23 15:23:47 -0500

    Omit filtering works and sanity checked

--- run summary.R
+++ run summary.R
@@ -10,11 +10,18 @@ omit_list = gsub("[[:space:]]", "", omit_list)
 omit_list = strsplit(omit_list,",")
 
 # filter lines out of data
-run_data_keep = run_data %>% dplyr::filter(!row_number() %in% omit_list)
+run_data_kept = run_data %>% dplyr::filter(!row_number() %in% omit_list[[1]])
 
+## Sanity Check ##
 
 # get number of trials omitted
 omit_count = length(omit_list[[1]]) %>% as.numeric()
+# calculate expected trials
+Trial_expected = total_trials - omit_count
+# Calculate # of kept trials
+Trials_kept = dplyr::count(run_data_kept) %>% as.numeric()
+# check
+if (Trials_kept != Trial_expected) stop("Error Omitting Trials")
 
 # Basic Stats -------------------------------------------------------------
 
