3ba0820a1d2d6aef84f07bc75987e9978baab9b9
Noelle James <github@nohelix.com>
2022-06-23 15:12:29 -0500

    Added code for omitting trials. Currently uses hard coded list for testing purposes.

--- run summary.R
+++ run summary.R
@@ -2,7 +2,19 @@
 # Omitted Trials ----------------------------------------------------------
 # These are typically demos or could trials that are ruled invalid by an observer.
 
+# get trial numbers to omit (these will be in a CSV format)
+omit_list = "2, 32, 6"
+# cleanse any spaces
+omit_list = gsub("[[:space:]]", "", omit_list)
+# split string into list
+omit_list = strsplit(omit_list,",")
 
+# filter lines out of data
+run_data_keep = run_data %>% dplyr::filter(!row_number() %in% omit_list)
+
+
+# get number of trials omitted
+omit_count = length(omit_list[[1]]) %>% as.numeric()
 
 # Basic Stats -------------------------------------------------------------
 
