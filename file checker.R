d895abcb6d6c41d79f04f569a1617410d1d16f5f
Noelle James <github@nohelix.com>
2022-06-24 11:46:13 -0500

    Keep Delay range and type for analysis checker.

--- file checker.R
+++ file checker.R
@@ -10,22 +10,31 @@ file_frequencies = unique(stim_master_list["Freq (kHz)"])
 
 file_frequency_ranges = stim_master_list %>%
   dplyr::filter(`Inten (dB)` != -100) %>% # Remove No-Go from range
-  dplyr::group_by(`Freq (kHz)`) %>%
-  dplyr::summarise(range = unique(`Inten (dB)`), .groups = 'drop') # Get each unique dB
+  dplyr::group_by(`Freq (kHz)`, `Delay (s)`, `Type`) %>%
+  dplyr::summarise(dB = unique(`Inten (dB)`), .groups = 'drop') # Get each unique dB
 
-file_intensity_range_steps = file_frequency_ranges %>%
-  dplyr::group_by(`Freq (kHz)`) %>%
-  dplyr::summarise(min = min(range),
-                   max = max(range),
-                   steps = range - lag(range, default = first(range)),
+file_summary = file_frequency_ranges %>%
+  dplyr::group_by(`Freq (kHz)`, `Delay (s)`, `Type`) %>%
+  dplyr::summarise(min = min(dB),
+                   max = max(dB),
+                   steps = dB - lag(dB, default = first(dB)),
                    .groups = 'keep') %>% # still grouped following this step, which is needed to remove the 1st row of each table that has a 0 step
   dplyr::slice(-1) %>% # Drop 1st row of each sub-table
   .[!duplicated(.), ] # reduce to unique rows. Should be 1 row per frequency unless something is screwed up
 
-# List of length of go sound - can be up to 3 values (50, 100, & 300) in our current file system
-file_duration = unique(stim_master_list["Dur (ms)"])
+# Check for mismatched step size
+if (length(unique(file_summary$steps)) != 1) {
+  warning(paste0("Missmatched step size in file.\nAction Required: Correct ", file_name, "\n"))
+  Warnings = append(Warnings, paste0("Action Required: Missmatched step size in file ", file_name))
+}
 
-file_response_window = unique(stim_master_list["Nose Out TL (s)"]) %>% as.numeric()
+# cleanup the duplicated table
+rm(file_frequencies)
+
+# List of length of go sound - can be up to 3 values (50, 100, & 300) in our current file system
+duration = unique(stim_master_list["Dur (ms)"])
+
+response_window = unique(stim_master_list["Nose Out TL (s)"]) %>% as.numeric()
 
 
 # Build 'real' file name --------------------------------------------------
