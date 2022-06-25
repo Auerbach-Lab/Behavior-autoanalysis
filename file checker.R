d980e554d7663e081bd25ce7c1052f7a059f8c06
Noelle James <github@nohelix.com>
2022-06-25 10:55:20 -0500

    Added check for auto-typing the analysis that should be done. Also moved step size check in to its own section.
    Note: this should be checked against the master data table.

--- file checker.R
+++ file checker.R
@@ -8,11 +8,13 @@
 # List of go sound frequencies
 file_frequencies = unique(stim_master_list["Freq (kHz)"])
 
+# Get ranges for each frequency
 file_frequency_ranges = stim_master_list %>%
   dplyr::filter(`Inten (dB)` != -100) %>% # Remove No-Go from range
-  dplyr::group_by(`Freq (kHz)`, `Delay (s)`, `Type`) %>%
+  dplyr::group_by(`Freq (kHz)`, `Delay (s)`, `Type`, `Repeat_number`) %>%
   dplyr::summarise(dB = unique(`Inten (dB)`), .groups = 'drop') # Get each unique dB
 
+# Make summary data table with step size
 file_summary = file_frequency_ranges %>%
   dplyr::group_by(`Freq (kHz)`, `Delay (s)`, `Type`) %>%
   dplyr::summarise(min = min(dB),
@@ -22,42 +24,77 @@ file_summary = file_frequency_ranges %>%
   dplyr::slice(-1) %>% # Drop 1st row of each sub-table
   .[!duplicated(.), ] # reduce to unique rows. Should be 1 row per frequency unless something is screwed up
 
+# cleanup the duplicated table
+rm(file_frequencies)
+
+
+# List of length of go sound - can be up to 3 values (50, 100, & 300) in our current file system
+duration = unique(stim_master_list["Dur (ms)"])
+
+# Get response window
+response_window = unique(stim_master_list["Nose Out TL (s)"]) %>% as.numeric()
+
+
+# Check dB step size ------------------------------------------------------
+
+dB_step_size = unique(file_summary$steps)
+
 # Check for mismatched step size
 if (length(unique(file_summary$steps)) != 1) {
   warning(paste0("Missmatched step size in file.\nAction Required: Correct ", file_name, "\n"))
   Warnings = append(Warnings, paste0("Action Required: Missmatched step size in file ", file_name))
 }
 
-# cleanup the duplicated table
-rm(file_frequencies)
 
-# List of length of go sound - can be up to 3 values (50, 100, & 300) in our current file system
-duration = unique(stim_master_list["Dur (ms)"])
+# Analysis Type ----------------------------------------------------------
+# Automatically attempt to determine analysis type. This should be checked against
+# master/power user data table.
 
-response_window = unique(stim_master_list["Nose Out TL (s)"]) %>% as.numeric()
+# For tonal files (octaves, or mainly 4-32kHz)
+if (stim_type == "tone") {
+  # Determine if octave file (in that case one of the normal intensity (dB) should be 0 or non-rewarded)
+  # Note that for each type 1 and type 0 the min & max should be equal (i.e. one intensity) but not necessarily between type 1 & 0
+  if (any(file_summary$Type == 0)) {analysis_type = "octave"}
+  # Determine if it has custom ranges (i.e. not all frequencies have the same range)
+  if ((length(unique(file_summary$min)) != 1 | length(unique(file_summary$max)) != 1)) {analysis_type = "custom tone"}
+  # Determine if all frequencies have the same range
+  if ((length(unique(file_summary$min)) == 1 & length(unique(file_summary$max)) == 1)) {analysis_type = "standard tone"}
+  else (stop("Unknown tonal file type."))
+}
 
+# For broadband files (training or otherwise)
+if (stim_type == "BBN") {
+  # Determine if training
+  if (file_summary$min == file_summary$max) {analysis_type = "BBN Training"}
+  else (analysis_type = "BBN")
+}
+
+# For Oddball files (training or otherwise)
+# TODO: This needs checking. I am betting on the if logical flow.
+if (stim_type == "train") {
+  # Determine if catch trials
+  if (any(file_summary$Type == 0)) {analysis_type = "Oddball with catch trials"}
+  # Determine if even odds
+  if (file_frequency_ranges %>%
+      dplyr::filter(Type != 0) %>% # Remove catch trials
+      dplyr::select(Repeat_number) %>%
+      unique() %>%
+      length() != 1) {analysis_type = "Oddball uneven trial odds"}
+  # Check for odds and catch trials
+  if (any(file_summary$Type == 0) & # Has catch trials
+      # Has uneven trial odds
+      file_frequency_ranges %>%
+      dplyr::filter(Type != 0) %>% # Remove catch trials
+      dplyr::select(Repeat_number) %>%
+      unique() %>%
+      length() != 1) {analysis_type = "Oddball with uneven trial & catch trials"}
+  else (stop("Unknown Oddball file type."))
+}
+
+if (!(exists("analysis_type"))) {stop("\nUnknown file type. Can not proceed with analysis")}
 
 # Build 'real' file name --------------------------------------------------
 
-# For standard tonal files
-if (analysis_type == "tone range") {
-  NULL
-}
-
-# For standard broadband files
-if (analysis_type == "BBN") {
-  NULL
-}
-
-# For standard broadband files
-if (analysis_type == "BBN training") {
-  NULL
-}
-
-# For Oddball files
-if (analysis_type == "Oddball") {
-  NULL
-}
 
 # Check File name vs. file ------------------------------------------------
 
