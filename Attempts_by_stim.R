310524c0370104eb219f4b0128053796c49b29a2
Noelle James <github@nohelix.com>
2022-07-13 08:32:16 -0500

    refactored averaging for summary table by stim for easier code reading and possible future expansion such as only include above threshold reaction times.

---
+++ Attempts_by_stim.R
@@ -0,0 +1,18 @@
+# Attempts ----------------------------------------------------------------
+# Get average attempts for each stim
+
+summary_attempts = dplyr::group_by(run_data_kept, Stim_ID) %>%
+  dplyr::summarise(Attempts_to_complete = mean(`Attempts_to_complete`) %>% round(digits = 1))
+
+# Add it to a summary table
+if (exists(run_data_summary)) {
+  run_data_summary = dplyr::left_join(x = run_data_summary,
+                                      y = summary_attempts,
+                                      by = "Stim_ID", all.x = TRUE)
+} else {
+  run_data_summary = dplyr::left_join(x = dplyr::select(stim_master_list, -Repeat_number),
+                                      y = summary_attempts,
+                                      by = "Stim_ID", all.x = TRUE)
+}
+
+rm(summary_attempts)
