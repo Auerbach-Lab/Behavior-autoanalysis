245a6328e1c5a67ff3fa7932153be2a511ade31d
tofof <github@tofof.com>
2022-08-12 12:53:17 -0500

    variable cleanup stubs

--- supervisor XLS export.R
+++ supervisor XLS export.R
@@ -19,6 +19,7 @@ setColWidths(wb, sheet = 1, cols = "A", widths = 18)
 s0 <- createStyle(fgFill = "darkgray", fontColour = "white", textDecoration = "bold")
 addStyle(wb, 1, s0, rows = 1:1, cols = 1:12, gridExpand = TRUE)
 saveWorkbook(wb, wbfilename, overwrite = TRUE) ## save to working directory
+#remove(wb, wbfilename, s0)
 
 # Manually apply filter ---------------------------------------------------
 
@@ -41,3 +42,4 @@ write_xml(x, xmlfile)
 files2zip <- dir(ziptemp, full.names = TRUE)
 zip(zipfile = wbfilename, dir(ziptemp, full.names = TRUE), mode = "cherry-pick", include_directories = FALSE)
 unlink(ziptemp, recursive = TRUE)   #delete temp directory
+#remove(x, xmlfile, autofilternode, nodes, nodetoadd, files2zip, ziptemp)
