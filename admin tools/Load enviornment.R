41ed25572338d247e6a279db51ba38d0f856a84b
Noelle James <github@nohelix.com>
2023-01-18 14:09:41 -0600

    Environment loaded.

---
+++ admin tools/Load enviornment.R
@@ -0,0 +1,23 @@
+InitializeMain <- function() {
+  Load_Packages <- function() {
+    # data loading external file formats
+    library(R.matlab);
+    
+    # data manipulation
+    library(tidyverse); library(dplyr); library(tidyr); library(rlang); library(stringr); library(purrr);
+    
+    # analysis & visualization
+    library(psycho); library(ggplot2);
+  }
+  
+  Load_Packages()
+  options(warn = 1) # we want to display warnings as they occur, so that it's clear which file caused which warnings
+  source("Z:/Behavior-autoanalysis/settings.R")  # hardcoded user variables
+  
+  rat_archive <<- read.csv(paste0(user_settings$projects_folder, "rat_archive.csv"), na.strings = c("N/A","NA"))
+  load(paste0(user_settings$projects_folder, "run_archive.Rdata"), .GlobalEnv)
+}
+
+
+# MAIN ---------------------------------------------------------
+InitializeMain()
