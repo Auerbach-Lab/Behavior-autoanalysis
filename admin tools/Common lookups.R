a86b3c41080486831ca87610ce10980027c491e0
Noelle James <github@nohelix.com>
2023-01-20 15:40:34 -0600

    Lookup Assignment & Weight
    also improved appearance of Threshold

--- admin tools/Common lookups.R
+++ admin tools/Common lookups.R
@@ -1,7 +1,20 @@
 
 # Thresholds --------------------------------------------------------------
-run_archive %>% filter(rat_name == "BP5") %>% 
-  select(date, rat_ID, stats) %>% 
+run_archive %>% filter(rat_name %in% c("GP6")) %>% 
+  select(date, rat_name, stats) %>% 
   unnest_wider(stats) %>% 
   unnest(threshold) %>% 
-  select(date, rat_ID, TH)
+  select(date, rat_name, TH)
+
+
+# Assignment --------------------------------------------------------------
+run_archive %>% filter(rat_name %in% c("Purple1")) %>% 
+  select(date, rat_name, assignment) %>% 
+  unnest_wider(assignment) %>% 
+  select(-comment)
+
+
+# Weight ------------------------------------------------------------------
+run_archive %>% filter(rat_name %in% c("Blue2")) %>% 
+  select(date, rat_name, weight)
+
