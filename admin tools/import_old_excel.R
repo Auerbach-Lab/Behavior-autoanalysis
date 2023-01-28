31043aac09c7b36d91fb381a0800eb395800a10a
Noelle James <github@nohelix.com>
2023-01-28 14:11:47 -0600

    Moved excel importer to admin tools, updated for loading files on 2023-01-28. Note that this does not include TTS as this hasn't been fully annotated yet.

--- admin tools/import_old_excel.R
+++ admin tools/import_old_excel.R
@@ -1,10 +1,11 @@
-require(openxlsx)
-require(dplyr)
-require(stringr)
+library(openxlsx)
+library(dplyr)
+library(stringr)
 
 
 names = c("Date", "Filename", "Weight", "Comments/Observations", "Experiment", "Phase", "Task", "Detail", "rat_name")
-folder = paste0(projects_folder, "Projects")
+folder = "Z:/Project Excels"
+depricated_date = "2023-01-17"
 
 readExcel <- function(filename, cols = c(1:3, 23, 28:31)) {
   filename = paste(folder, filename, sep = "/")
@@ -17,10 +18,16 @@ readExcel <- function(filename, cols = c(1:3, 23, 28:31)) {
 }
 
 old_excel_archive = setNames(data.frame(matrix(ncol = 9, nrow = 0)), names) %>%
+  # rbind(readExcel("Noise_TTS_Gp1_Green-Orange.xlsx")) %>%
   rbind(readExcel("Fmr1-LE_Grp1_BP_LP_Purple.xlsx")) %>%
-  rbind(readExcel("Noise_TTS_Gp1_Green-Orange.xlsx")) %>%
   rbind(readExcel("Tsc2_Eker_Gp1_RP_GP_TP.xlsx")) %>%
+  rbind(readExcel("GD_Grp1_Red_Teal.xlsx")) %>%
   rbind(readExcel("Oddball Pilot1 Blue1-4.xlsx", cols = c(1:3, 36, 48:51)))
 
+old_excel_archive = old_excel_archive %>%
+  filter(! Filename %in% c("Did not run", "Handling")) %>%
+  filter(Date < depricated_date)
 
-rm(names, folder, readExcel)
+save(old_excel_archive, file = paste0(projects_folder, "old_excel_archive.Rdata"), ascii = TRUE, compress = FALSE)
+
+rm(list = c("names", "folder", "readExcel", "depricated_date"))
