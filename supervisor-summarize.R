015003e490100b9c2c7c908e25c7cfbfa7b209a5
tofof <github@tofof.com>
2023-02-21 14:15:45 -0600

    move trials archive uuid check and backup

--- supervisor-summarize.R
+++ supervisor-summarize.R
@@ -16,51 +16,6 @@ InitializeWriter <- function() {
   load(paste0(projects_folder, "run_archive.Rdata"), .GlobalEnv)
 }
 
-Check_Trial_Archives <- function () {
-  Archive_Reader <- function(df) {
-    archive_file = df["file"]
-    experiment = df["experiment"]
-    cat(glue("\t{experiment}..."))
-    archive = fread(archive_file)
-    r = 0
-
-    # Check file
-    # TODO sum expected number of trials and make sure the number of lines matches
-    if (nrow(archive) >= 2 & length(names(archive)) >= 18) {
-      r = unique(archive$UUID)
-      cat(glue(" {length(r)} entries..."))
-    } else {
-      stop(glue("ABORT: Problem with {archive_file}."))
-    }
-
-    # Backup
-    fwrite(archive, file = paste0(archive_file, ".backup"))
-    cat(" backed up\n")
-    return(r)
-  }
-
-  # Get archives
-  archives = list.files(path = projects_folder, pattern = "^.*_archive.csv.gz$") %>% paste0(projects_folder, .) %>%
-    data.frame(file = .) %>% mutate(experiment = str_extract(file, pattern = '(?<=s/).*(?=_a)'))     # Get just the experiment name
-  writeLines("Checking and backing up trials archives")
-  if(nrow(archives) == 0) writeLines("Nothing to backup")
-  else {
-    trials_uuids <<- do.call(c, apply(archives, 1, Archive_Reader))
-    trials_uuid_counts = length(trials_uuids)
-    runs_uuid_counts = length(unique(run_archive$UUID))
-
-    if (trials_uuid_counts == runs_uuid_counts) {
-      writeLines(glue("Runs and trials agree: {trials_uuid_counts} UUIDs."))
-    } else {
-      difference = setdiff(trials_uuids, unique(run_archive$UUID))
-      writeLines("The following UUIDs were in one but not the other:")
-      writeLines(glue("\t{difference}"))
-      warn = glue("ACTION REQUIRED: {trials_uuid_counts} UUIDs found across trials archives, but {runs_uuid_counts} found in run_archive.")
-      warning(paste0(warn, "\n"))
-    }
-  }
-}
-
 Workbook_Writer <- function() {
   Define_Styles <- function() {
     r = list(
@@ -957,7 +912,7 @@ Workbook_Writer <- function() {
 # Workflow -----------------------------------------------------------
 
 InitializeWriter()
-Check_Trial_Archives()
+source(paste0(projects_folder, "admin tools\\check UUIDs and backup trials archives.R"))
 Workbook_Writer()
 rm(list = c("experiment_config_df", "run_today", "wb"))
 
