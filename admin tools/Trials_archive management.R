3f4555e891339aa75fc55dd8bf6454f16402e567
Noelle James <github@nohelix.com>
2023-03-10 09:20:47 -0600

    Major rewrite of Trials Archive checking Not complete but includes an overall trial count check and if needed a run by run trial check.
    Needs to be integrated with remove trial.R and import old data.R to remove and reload bad runs.

--- admin tools/Trials_archive management.R
+++ admin tools/Trials_archive management.R
@@ -1,60 +1,83 @@
-group = "Fmr1-LE"
+Find_Issues_in_Archives <- function(group) {
+  cat(glue("Checking {group}..."))
+  # Load file
+  trials_archive = fread(paste0(projects_folder, group, "_archive.csv.gz"))
+  
+  # Get list of entered trials
+  UUIDs_in_trials = trials_archive$UUID %>% unique()
+  # Get list of entered runs
+  InitializeMain()
+  UUIDs_in_runs = run_archive %>% unnest_wider(assignment) %>% filter(experiment == group) %>% .$UUID %>% unique()
+  trial_counts = run_archive %>% unnest_wider(assignment) %>% filter(experiment == group) %>% transmute(trials = map_dbl(stats, ~.$trial_count), UUID = UUID)
+  trials_total = sum(trial_counts$trials)
+  
+  if(nrow(trials_archive) != trials_total) {
+    cat(glue(" Issue with archive: off by {abs(trials_total-nrow(trials_archive))}"))
+    
+    trial_count_comparison = trials_archive %>% group_by(UUID) %>% summarise(rows = length(UUID)) %>%
+      left_join(trial_counts, by = "UUID") %>%
+      mutate(Issue = rows != trials)
+    
+    Bad_UUIDs = filter(trial_count_comparison, Issue == TRUE) %>% left_join(run_archive %>% select(date, rat_name, rat_ID, file_name, UUID), by = "UUID")
+    
+    if(nrow(trial_count_comparison) == 0) {
+      writeLines(glue("ISSUE IN {group}_archive.csv.gz"))
+    } else {
+      glue_data(Bad_UUIDs)
+      
+      
+      # De-duplicating ----------------------------------------------------------
+      
+      # Remove
+      trials_archive_dedup = unique(trials_archive)
+      
+      if(nrow(trials_archive_dedup) != nrow(trials_archive)) {
+        writeLines("Duplicate entries found in trials archive")
+        switch (menu(c("Yes", "No"), 
+                     title = glue("Write out new {group}_archive?
+                                    Removing {nrow(trials_archive)-nrow(trials_archive_dedup)} trials. Number of trials will now be {nrow(trials_archive_dedup)} instead of {nrow(trials_archive)}"), 
+                     graphics = FALSE),
+                # 1 (Yess): Write file
+                fwrite(trials_archive_dedup, paste0(projects_folder, group, "_archive.csv.gz")),
+                # 2 (No): Abort
+                writeLines("Aborting. No changes made")
+        )
+        
+        # Finding missing runs ----------------------------------------------------
+        # Find what is missing
+        UUIDs_missing = keep(UUIDs_in_runs, ~ ! . %in% UUIDs_in_trials)
+        UUIDs_without_runs = keep(UUIDs_in_trials, ~ ! . %in% UUIDs_in_runs)
+        
+        if(is_empty(UUIDs_without_runs)) {
+          writeLines(glue("\tNo extra data for {group}"))
+          rm(UUIDs_without_runs)
+        } else {
+          writeLines("Removing orphan trial data... ")
+          trials_archive = filter(trials_archive, ! UUID %in% UUIDs_without_runs)
+          fwrite(trials_archive, file = paste0(projects_folder, group, "_archive.csv.gz"))
+          cat("Done.")
+          rm(UUIDs_without_runs)
+        }
+        
+        # Write out list to load
+        temp = run_archive %>% filter(UUID %in% UUIDs_missing) %>% 
+          arrange(date) %>%
+          select(date, rat_name, rat_ID, omit_list, assignment) %>% 
+          unnest_wider(assignment) %>% 
+          unnest(omit_list) %>%
+          mutate(Dataset = group)
+        
+        if(nrow(temp) >= 1) {
+          writeLines(glue("\tMissing data for {group}"))
+          fwrite(temp, "missing.csv")
+        } else { writeLines(glue("\tNo missing data for {group}")) }
+      }
+    }
+    
 
-# Find missing entries ---------------------------------------------------
-# Load file
-trials_archive = fread(paste0(projects_folder, group, "_archive.csv.gz"))
-
-# Get list of entered trials
-UUIDs_in_trials = trials_archive$UUID %>% unique()
-# Get list of entered runs
-InitializeMain()
-UUIDs_in_runs = run_archive %>% unnest_wider(assignment) %>% filter(experiment == group) %>% .$UUID %>% unique()
-# Find what is missing
-UUIDs_missing = keep(UUIDs_in_runs, ~ ! . %in% UUIDs_in_trials)
-UUIDs_without_runs = keep(UUIDs_in_trials, ~ ! . %in% UUIDs_in_runs)
-
-if(is_empty(UUIDs_without_runs)) {
-  writeLines(glue("\tNo extra data for {group}"))
-  rm(UUIDs_without_runs)
-} else {
-  writeLines("Removing orphan trial data... ")
-  trials_archive = filter(trials_archive, ! UUID %in% UUIDs_without_runs)
-  fwrite(trials_archive, file = paste0(projects_folder, group, "_archive.csv.gz"))
-  cat("Done.")
-  rm(UUIDs_without_runs)
+  } else cat(" all good\n")
 }
 
-# Write out list to load
-temp = run_archive %>% filter(UUID %in% UUIDs_missing) %>% 
-  arrange(date) %>%
-  select(date, rat_name, rat_ID, omit_list, assignment) %>% 
-  unnest_wider(assignment) %>% 
-  unnest(omit_list) %>%
-  mutate(Dataset = group)
-
-if(nrow(temp) >= 1) {
-  writeLines(glue("\tMissing data for {group}"))
-  fwrite(temp, "missing.csv")
-  } else { writeLines(glue("\tNo missing data for {group}")) }
-  
-
-rm(list = c("trials_archive", "group", "UUIDs_in_trials", "UUIDs_in_runs", "UUIDs_missing", "temp"))
-
-
-# De-duplicating ----------------------------------------------------------
-
-# Load file
-trials_archive = fread(paste0(projects_folder, group, "_archive.csv.gz"))
-
-# ID duplicates
-Dup = duplicated(trials_archive) | duplicated(trials_archive, fromLast = TRUE)
-trials_archive_dup = mutate(trials_archive, Dup = Dup) %>% rowid_to_column()
-filter(trials_archive_dup, Dup == TRUE)
-
-# Remove
-trials_archive_dedup = unique(trials_archive)
-
-# Write file
-fwrite(trials_archive_dedup, paste0(projects_folder, "Tsc2-LE_archive.csv.gz"))
-
-rm(list = c("trials_archive", "group", "Dup", "trials_archive_dup", "trials_archive_dedup"))
+list.files(path = projects_folder, pattern = "^.*_archive.csv.gz$") %>% paste0(projects_folder, .) %>%
+  data.frame(file = .) %>% mutate(experiment = str_extract(file, pattern = '(?<=s/).*(?=_a)')) %>% .$experiment %>%
+  lapply(Find_Issues_in_Archives)
