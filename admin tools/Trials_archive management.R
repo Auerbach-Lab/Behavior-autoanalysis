fea54f422ce23fd7a4648edf194d6328a110f833
Noelle James <github@nohelix.com>
2023-03-10 11:26:07 -0600

    Continued major rewrite, still in-progress Now should try to handle autoloading orphaned trials that have been wiped from the system.
    In process of writting orphan run checker and loader.

--- admin tools/Trials_archive management.R
+++ admin tools/Trials_archive management.R
@@ -1,3 +1,12 @@
+
+# User Variables ----------------------------------------------------------
+
+# slashes must be either / or \\
+modern_matlab_file_location = "Z:/Daily Matlab files"
+Box_file_location = "C:/Users/Noelle/Box/Behavior Lab/Projects (Behavior)"
+
+
+# Functions ---------------------------------------------------------------
 Find_Issues_in_Archives <- function(group) {
   cat(glue("Checking {group}..."))
   # Load file
@@ -21,63 +30,105 @@ Find_Issues_in_Archives <- function(group) {
     Bad_UUIDs = filter(trial_count_comparison, Issue == TRUE) %>% left_join(run_archive %>% select(date, rat_name, rat_ID, file_name, UUID), by = "UUID")
     
     if(nrow(trial_count_comparison) == 0) {
-      writeLines(glue("ISSUE IN {group}_archive.csv.gz"))
+      writeLines(glue("ISSUE IN {group}_archive.csv.gz
+                          NOT backed up"))
     } else {
       glue_data(Bad_UUIDs)
       
+      # Finding missing runs ----------------------------------------------------
+      # Find what is missing
+      UUIDs_missing = keep(UUIDs_in_runs, ~ ! . %in% UUIDs_in_trials)
+      UUIDs_without_runs = keep(UUIDs_in_trials, ~ ! . %in% UUIDs_in_runs)
       
-      # De-duplicating ----------------------------------------------------------
-      
-      # Remove
-      trials_archive_dedup = unique(trials_archive)
-      
-      if(nrow(trials_archive_dedup) != nrow(trials_archive)) {
-        writeLines("Duplicate entries found in trials archive")
-        switch (menu(c("Yes", "No"), 
-                     title = glue("Write out new {group}_archive?
-                                    Removing {nrow(trials_archive)-nrow(trials_archive_dedup)} trials. Number of trials will now be {nrow(trials_archive_dedup)} instead of {nrow(trials_archive)}"), 
-                     graphics = FALSE),
-                # 1 (Yess): Write file
-                fwrite(trials_archive_dedup, paste0(projects_folder, group, "_archive.csv.gz")),
-                # 2 (No): Abort
-                writeLines("Aborting. No changes made")
-        )
+      if(is_empty(UUIDs_without_runs)) {
+        writeLines(glue("No extra data for {group}"))
+      } else {
+        writeLines("Removing orphan trial data... ")
+        trials_archive = filter(trials_archive, ! UUID %in% UUIDs_without_runs)
+        fwrite(trials_archive, file = paste0(projects_folder, group, "_archive.csv.gz"))
+        cat("Done.")
         
-        # Finding missing runs ----------------------------------------------------
-        # Find what is missing
-        UUIDs_missing = keep(UUIDs_in_runs, ~ ! . %in% UUIDs_in_trials)
-        UUIDs_without_runs = keep(UUIDs_in_trials, ~ ! . %in% UUIDs_in_runs)
+        # Find and load orphan trials -----------------------------------------------
         
-        if(is_empty(UUIDs_without_runs)) {
-          writeLines(glue("\tNo extra data for {group}"))
-          rm(UUIDs_without_runs)
-        } else {
-          writeLines("Removing orphan trial data... ")
-          trials_archive = filter(trials_archive, ! UUID %in% UUIDs_without_runs)
-          fwrite(trials_archive, file = paste0(projects_folder, group, "_archive.csv.gz"))
-          cat("Done.")
-          rm(UUIDs_without_runs)
+        Load_old_file_from_UUID <- function(UUID) {
+          # Get date & time from UUID to look for files
+          bad_date = stringr::str_sub(UUID, start = 1, end = 8)
+          bad_time = stringr::str_sub(UUID, start = 9, end = 14)
+          
+          # Determine project from the group we are in
+          if (group == "Tsc2-LE") project = "Tsc2 Eker"
+          else if (group == "GD") project = "Tsc2 Eker" # some of the earliest data ended up here
+          else project = group
+          
+          # find location of bad file
+          if(bad_date > 20230104) file_location = modern_matlab_file_location
+          else file_location = glue("{Box_file_location}/{project}/data")
+          
+          # Get possible files
+          files = list.files(paste0(file_location, "/", bad_date), pattern = paste0(bad_date, "-", bad_time, "_BOX#\\d{3}\\.mat$"), recursive = TRUE)
+          cat(glue("
+                        Found {length(files)} possible files..."))
+          
+          # Check for old assignments
+          old_assignment_check <- function(file) {
+            load(paste0(projects_folder, "old_excel_archive.Rdata"))
+            
+            rat = stringr::str_extract(file, pattern = "^.+?[:digit:]+?(?=_)") 
+              # Regex: one or more character (.*+?, ? makes it lazy so first match) 
+              # followed by one or more digits ([:digit:]+?), followed by but NOT including an _
+        
+            old_data = old_excel_archive %>% dplyr::filter(Date == lubridate::ymd(bad_date) & rat_name == rat)
+            
+            if(nrow(old_data) == 1) {
+              cat(" with data to load")
+              return(file)
+            } else {
+              return(NULL)
+            }
+            
+          }
+          
+          files_checked = lapply(files, old_assignment_check)
+          files_checked = discard(files_checked, is_null)
+          
+          if(length(files_checked) > 0) {
+            cat(glue(" attempting to load {length(files_checked)}"))
+            source(paste0(projects_folder, "main.R"))
+            lapply(files_checked, Process_File, old_file = TRUE, ignore_name_check = TRUE, exclude_trials = "")
+          } else cat(glue("no data found. Can NOT autoload run for
+                          {UUID}"))
         }
         
-        # Write out list to load
-        temp = run_archive %>% filter(UUID %in% UUIDs_missing) %>% 
-          arrange(date) %>%
-          select(date, rat_name, rat_ID, omit_list, assignment) %>% 
-          unnest_wider(assignment) %>% 
-          unnest(omit_list) %>%
-          mutate(Dataset = group)
+        writeLines(paste0("\nLoading ", length(UUIDs_without_runs), " files."))
+        # try to get files and then run through main
+        lapply(UUIDs_without_runs, Load_old_file_from_UUID)
+        writeLines(glue("Done attempting to load missing runs"))
         
-        if(nrow(temp) >= 1) {
-          writeLines(glue("\tMissing data for {group}"))
-          fwrite(temp, "missing.csv")
-        } else { writeLines(glue("\tNo missing data for {group}")) }
       }
-    }
-    
+      
 
-  } else cat(" all good\n")
+      # Find and load orphan runs -----------------------------------------------
+      
+      if(length(UUIDs_missing) > 0) writeLines(glue("Missing trials for runs in {group}"))
+      else writeLines(glue("No missing data for {group}"))
+      
+      # Write out list to load
+      bad_runs = run_archive %>% filter(UUID %in% UUIDs_missing) %>% arrange(date)
+      
+      ## TODO: in-progress
+      # 1) need to backup data but not restore assignment through remove entry.R AND save the key_data for autoloading
+      # This will need an apply with 1 for rows and will require that the remove entry be in a 'safetied' state
+      # 2) reload data from key_data using main with the assignment from the key_data
+
+    }
+  } else {
+    cat(" good for backup...")
+    # Backup
+    fwrite(archive, file = paste0(projects_folder, group, "_archive.csv.gz.backup"))
+    cat(" backed up\n")
+  }
 }
 
 list.files(path = projects_folder, pattern = "^.*_archive.csv.gz$") %>% paste0(projects_folder, .) %>%
-  data.frame(file = .) %>% mutate(experiment = str_extract(file, pattern = '(?<=s/).*(?=_a)')) %>% .$experiment %>%
+  data.frame(file = .) %>% mutate(experiment = stringr::extract(file, pattern = '(?<=s/).*(?=_a)')) %>% .$experiment %>%
   lapply(Find_Issues_in_Archives)
