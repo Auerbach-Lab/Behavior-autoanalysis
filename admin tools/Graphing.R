959da62ea193f83e24dd49cb4106b8405e57e06c
Noelle James <github@nohelix.com>
2023-03-10 17:33:55 -0600

    Step size now shown on graphing

--- admin tools/Graphing.R
+++ admin tools/Graphing.R
@@ -32,18 +32,33 @@ Grapher <- function(rat_to_graph) {
       filter(analysis_type == today_analysis_type) %>%
       filter(Freq == Freq_filter) 
   }
-
   
+  Get_Step_Size <- function(summary) {
+    temp = summary$dB_step_size %>% unique
+    
+    if(length(temp) != 1) {
+      step_size = Inf
+      # print(temp)
+    } else step_size = temp
+    
+    return(step_size)
+  }
+  
+  data = data %>% rowwise() %>% mutate(step_size = Get_Step_Size(summary) %>% as.factor())
   
   suppressMessages(print(ggplot(data = data, aes(x = dB, y = value)) +
       {if(what_to_graph == "dprime") geom_hline(yintercept = 1.5, color = "blue") } +
-      geom_point(color = "black") +
-      geom_smooth(se = FALSE, linewidth = 2, color = "grey") +
       geom_line(data = Today,
                 linewidth = 2, color = "darkgreen") +
+      geom_point(aes(color = step_size)) +
+      geom_smooth(se = FALSE, linewidth = 2, color = "grey") +
+        # geom_boxplot(aes(fill = step_size, group = interaction(dB, step_size)), position = position_dodge(4), width = 1, show.legend = FALSE) +
+      {if(what_to_graph == "reaction") stat_summary(aes(color = step_size), fun = mean, shape = 18, position = position_dodge(4), size = 1) } +
       labs(title = glue("{what_to_graph} for {rat_to_graph} @ {Freq_filter}kHz"),
-           y = paste(what_to_graph)) +
+           y = paste(what_to_graph),
+           color = "Intensity\nSteps") +
       scale_x_continuous(breaks = seq(0, 100, by = 20)) +
+      scale_color_manual(values = c("10" = "black", "5" = "coral")) +
       theme_ipsum_es()))
 }
 
