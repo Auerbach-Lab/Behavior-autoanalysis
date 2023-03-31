57d17dc3ac22d2eb27223747e7f126b0decb56fb
Noelle James <github@nohelix.com>
2023-03-31 15:07:50 -0500

    General improvements to weight graphing Better commenting Now 3 weeks instead of one month with dates every 2 days on x-axis. Added points for 3 most recent trial counts (note this is does not directly correlate to days as multiple files can happen on one day. This could be changed by doing a date filter but doesn't seem necessary.

--- admin tools/Graphing (Weight).R
+++ admin tools/Graphing (Weight).R
@@ -8,7 +8,7 @@ Weight_Grapher <- function(rat_to_graph) {
   max_weight_runs = max(rat_runs$weight, na.rm = TRUE)
   max_weight_freefeed = dplyr::filter(rat_archive, Rat_ID == rat_to_graph)$Max_Weight
   max_weight = max(max_weight_runs, max_weight_freefeed)
-  rat_runs = rat_runs %>% filter(date > str_remove_all(Sys.Date()-30, "-")) %>%
+  rat_runs = rat_runs %>% filter(date > str_remove_all(Sys.Date()-21, "-")) %>%
     mutate(date_asDate = lubridate::ymd(date),
            trial_count = map_dbl(stats, ~.$trial_count),
            weight_change = (weight - max_weight)/max_weight,
@@ -25,24 +25,33 @@ Weight_Grapher <- function(rat_to_graph) {
   suppressMessages(print(
       rat_runs %>% 
       ggplot(aes(x = date_asDate, y = weight)) +
+        # min trials line
       geom_hline(yintercept = min_trials, linetype = "longdash") +
-      geom_point(aes(x = min(date_asDate), y = max_weight), fill = "black", size = 5, position = position_nudge(x = -0.5)) +
       geom_text(aes(x = min(date_asDate), y = min_trials - 5), label = "Minimum trials")+
+        # max weight point
+      geom_point(aes(x = min(date_asDate), y = max_weight), fill = "black", size = 5, position = position_nudge(x = -0.5)) +
+        # weight trend line
       geom_smooth(color = "grey", linewidth = 2,
                   se = FALSE, na.rm = TRUE, method = "lm", formula= y~x) +
       geom_point(shape=21, color="black", fill="#69b3a2", size=5) +
       geom_text(aes(x = min(date_asDate), y = annotation_x), nudge_x = -0.5, label = "Weight", color="#69b3a2")+
+        # trial trend line (Note we don't show all the trial point because they have great variation)
       geom_smooth(aes(x = date_asDate, y = trial_count),
                   color = "black", linewidth = 2,
                   se = FALSE, na.rm = TRUE, method = "lm", formula= y~x) +
-      geom_text(data = filter(rat_runs, !is.na(weight_annotation)),
-                aes(y = annotation_x + 6, label = weight_annotation), color="darkred", size = 3)+
-      geom_point(data = tail(rat_runs, n = 3), aes(y = trial_count)) +
       geom_text(aes(x = min(date_asDate), y = trails_label_x + 6), label = "Trial trend")+
-      ggtitle(glue("{unique(rat_runs$rat_name)} two week Weight vs. Trail count")) +
+        # annotations to weight points only if weight is 15%+ off max weight (Note we want this on top of other layers)
+      geom_text(data = filter(rat_runs, !is.na(weight_annotation)),
+                aes(y = annotation_x + 6, label = weight_annotation), color="darkred", size = 3) +
+        # 3 most recent trial counts used to see if pulling up or down the trend
+      geom_point(data = tail(arrange(rat_runs, date_asDate), n = 3), aes(y = trial_count), color = "blue", show.legend = FALSE) +
+        # lower the bottom of the y scale as it can be anchored to minimum trials and the label is bellow the line
       scale_y_continuous(expand = c(0.1, 0)) +
-      theme_ipsum_es() +
-      labs(x = "Date", y = "Weight & Trial Count")
+      scale_x_date(date_breaks = "4 day", date_minor_breaks = "2 day",
+                   date_labels = "%m-%d") +
+      labs(title = glue("{unique(rat_runs$rat_name)} three week Weight vs. Trail count"),
+           x = "Date", y = "Weight & Trial Count") +
+      theme_ipsum_es()
     ))
 }
 
@@ -52,8 +61,8 @@ rat_archive %>%
   #select rats that have NOT been run today
   # filter(Assigned_Filename != "") %>%
   # filter(Assigned_Experiment  == "Tsc2-LE") %>%
-  # filter(!Rat_name %in% c("Teal3", "Teal4")) %>%
-  filter(str_detect(Box, "2.")) %>%
+  # filter(Rat_name %in% c("RP5", "RP6")) %>%
+  filter(str_detect(Box, "5.")) %>%
   arrange(desc(Box)) %>%
   .$Rat_ID %>%
   lapply(Weight_Grapher)
