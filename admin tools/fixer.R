be68134de6cf0e6da984ca6db51a6b57f0ca488e
Noelle James <github@nohelix.com>
2023-04-19 12:05:37 -0500

    Bug fix: comment is now optional
    also commented out the post save check.

--- admin tools/fixer.R
+++ admin tools/fixer.R
@@ -2,12 +2,13 @@ fixer <- function(tableRow, newValue) {
   InitializeMain()
   
   df = run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment)
+    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-any_of(comment))
   
-  run_archive[tableRow,]$assignment[[1]]$detail = newValue
+  # set value to change
+  run_archive[tableRow,]$assignment[[1]]$task = newValue
   
   df = bind_rows(df, run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment))
+    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-any_of(comment)))
   
   print(df)
   
@@ -22,9 +23,22 @@ fixer <- function(tableRow, newValue) {
          # 2 (No): Abort
          {outcome = "No change"})
   
-  InitializeMain()
-  run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment) %>% print
+  # InitializeMain()
+  # run_archive[tableRow,] %>% 
+  #   select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment) %>% print
   
   return(outcome)
 }
+
+
+# filter to rowid
+run_archive %>% rowid_to_column %>% filter(rat_name %in% c("RP3")) %>% 
+  unnest_wider(assignment) %>%
+  unnest_wider(stats) %>% arrange(desc(date)) %>%
+  mutate(hit_percent = hit_percent * 100, FA_percent = FA_percent * 100, 
+         BG = str_extract(file_name, pattern = "(?<=BG_PKN_).*(?=dB)")) %>% 
+  filter(BG == 30) %>% 
+  .$rowid
+
+# then lapply
+lapply(fixer, newValue = "whatever")
