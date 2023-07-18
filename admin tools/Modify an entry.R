cab2f331115510bdb17a2732ca258e1df3ff262d
Noelle James <github@nohelix.com>
2023-07-18 09:30:32 -0500

    Added new Feeding notes mod
    Ability to modify the "weightProblem" field. This is typically necessary when a supervisor makes an execuative choice to provide more food.

--- admin tools/Modify an entry.R
+++ admin tools/Modify an entry.R
@@ -1,7 +1,7 @@
 InitializeMain()
 
 # Find an entry -----------------------------------------------------------
-row_to_modify = run_archive %>% rowid_to_column() %>% filter(date == "20230428" & rat_name == "BP3")
+row_to_modify = run_archive %>% rowid_to_column() %>% filter(date == "20230713" & rat_name == "Purple4")
 row_to_modify %>% select(date, rat_name, rat_ID, weight, scientist, rxnProblem, weightProblem)
 line_to_modify = row_to_modify$rowid
 
@@ -12,7 +12,7 @@ line_to_modify = row_to_modify$rowid
 run_archive[line_to_modify,]$invalid #= "TRUE"
 
 # Assignment change -------------------------------------------------------
-run_archive[line_to_modify,] %>% select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment)
+run_archive[line_to_modify,] %>% select(date, rat_name, rat_ID, file_name, assignment) %>% unnest_wider(assignment) %>% select(-comment)
 run_archive[line_to_modify,]$assignment[[1]]$phase #= "Tones"
 run_archive[line_to_modify,]$assignment[[1]]$task #= "Rxn"
 run_archive[line_to_modify,]$assignment[[1]]$detail #= "Mixed"
@@ -24,6 +24,12 @@ run_archive[line_to_modify,]$weight #= 239
 
 # Warnings list modification ----------------------------------------------
 run_archive[line_to_modify,]$warnings_list[[1]] #= run_archive[line_to_modify,]$warnings_list[[1]][-1]
+
+
+# Feeding or Rxn notes modification ---------------------------------------
+run_archive[line_to_modify,] %>% select(date, rat_name, rat_ID, file_name, scientist, rxnProblem, weightProblem)
+run_archive[line_to_modify,]$weightProblem = paste0(run_archive[line_to_modify,]$weightProblem,
+                                                    "NJ: Weight dropping so gave an extra male pellet")
 
 
 # Save modification -------------------------------------------------------
