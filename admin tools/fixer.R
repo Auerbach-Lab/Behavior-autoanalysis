f3df19fa8218b60df81c21ce8c491a94fd8129c4
Noelle James <github@nohelix.com>
2024-01-23 10:44:49 -0600

    More flexbility as takes both task and detail

--- admin tools/fixer.R
+++ admin tools/fixer.R
@@ -1,14 +1,17 @@
-fixer <- function(tableRow, newValue) {
-  InitializeMain()
+fixer <- function(tableRow, newValue_task, newValue_detail) {
+  
+  # print(newValue_task)
+  # print(newValue_detail)
   
   df = run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment)
+    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment)
   
   # set value to change
-  run_archive[tableRow,]$assignment[[1]]$task = newValue
+  if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task = newValue_task
+  if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail = newValue_detail
   
   df = bind_rows(df, run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment))
+    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment))
   
   print(df)
   
@@ -16,9 +19,9 @@ fixer <- function(tableRow, newValue) {
               title="Save modified entry?", 
               graphics = TRUE),
          # 1 (Yes): Write file
-         {
-           # Save modification -------------------------------------------------------
-           save(run_archive, file = paste0(projects_folder, "run_archive.Rdata"), ascii = TRUE, compress = FALSE)
+         {# Push modification -------------------------------------------------------
+           if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task <<- newValue_task
+           if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail <<- newValue_detail
            outcome = "Fixed"},
          # 2 (No): Abort
          {outcome = "No change"})
@@ -29,15 +32,21 @@ fixer <- function(tableRow, newValue) {
   
   return(outcome)
 }
-
+InitializeMain()
 
 # filter to rowid
-run_archive %>% rowid_to_column %>% filter(rat_name %in% c("RP3")) %>% 
+run_archive %>% rowid_to_column %>% 
+  filter(rat_name %in% c("Bronze3", "Bronze4")) %>%
   unnest_wider(assignment) %>%
   arrange(desc(date)) %>%
-  mutate(BG = str_extract(file_name, pattern = "(?<=BG_PKN_).*(?=dB)")) %>% 
-  filter(BG == 30) %>% 
-  .$rowid
+  filter(date %in% c(20240113, 20240112, 20240111, 20240110)) %>%
+  # mutate(BG = str_extract(file_name, pattern = "(?<=BG_PKN_).*(?=dB)")) %>% 
+  # filter(task == "Reset")
 
 # then lapply
-lapply(fixer, newValue = "whatever")
+  .$rowid %>%
+  lapply(fixer, newValue_task = NA, newValue_detail = "Alone")
+
+# Save modification -------------------------------------------------------
+save(run_archive, file = paste0(projects_folder, "run_archive.Rdata"), ascii = TRUE, compress = FALSE)
+print("Saved")
