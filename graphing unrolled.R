39e9068cbb244df7e77318b8c0664e48cb7ba31e
Noelle James <github@nohelix.com>
2024-02-02 14:02:28 -0600

    Grapher sane x-axis breaks + formating cleanup
    Note that this introduces a new message when graphing Oddball because we override a declaired scale.

--- graphing unrolled.R
+++ graphing unrolled.R
@@ -4,13 +4,13 @@ Generate_Graph <- function(rat_name, ratID) {
   # TODO:
   # This code does not currently differentiate pre vs. post hearing loss/surgery/etc
   # You can handle this but it requires access to rat_archive, which I believe we have.
-
+  
   # TODO:
   # It would be best if this code looked up the rat name for itself
   # It'd only need ratID that way, and could never be out of sync
-
+  
   # Functions ---------------------------------------------------------------
-
+  
   Range_Grapher <- function(plot) {
     # Range Graph for points
     plot +
@@ -30,7 +30,7 @@ Generate_Graph <- function(rat_name, ratID) {
       theme_ipsum_es() +
       theme(legend.position = "bottom")
   }
-
+  
   Line_Grapher <- function(plot) {
     # Line Graph for Curves
     plot +
@@ -45,11 +45,12 @@ Generate_Graph <- function(rat_name, ratID) {
       geom_point(data = today_graph_data, shape=21, color="black", fill = "mediumslateblue", size = 5) +
       ggtitle(glue("{rat_name} {str_to_title(what_to_graph)} Curve Check")) +
       scale_color_manual(values = c("Today" = "mediumslateblue", "Average" = "thistle"), name = "") +
+      scale_x_continuous(breaks = seq(from = -20, to = 100, by = 20)) +
       coord_cartesian(clip = "off") +
       theme_ipsum_es() +
       theme(legend.position = "bottom")
   }
-
+  
   Blank_Grapher <- function(plot) {
     plot +
       xlim(0, 100) +
@@ -58,18 +59,18 @@ Generate_Graph <- function(rat_name, ratID) {
       theme_ipsum_es() +
       theme(legend.position = "bottom")
   }
-
+  
   # Graph Data generation ---------------------------------------------------
   rat_runs = run_archive %>% dplyr::filter(rat_ID == ratID)
   rat_runs = rat_runs %>% mutate(date_asDate = lubridate::ymd(date)) %>% arrange(desc(date))
-
+  
   # Check to see if rat has had Hearing loss, surgery or any other procedure
   is_post_baseline = !is.na(filter(rat_archive, Rat_ID == ratID)$HL_date)
-
+  
   # only want to graph relevant data so need today's data
   today_data = rat_runs %>%
     head(n = 1) # this should probably be a filter date, or like, select(max) or something.
-
+  
   current_analysis_type = today_data$analysis_type
   current_phase = pluck(today_data, "assignment", 1, "phase")
   current_task = pluck(today_data, "assignment", 1, "task")
@@ -77,7 +78,7 @@ Generate_Graph <- function(rat_name, ratID) {
   current_durations = pluck(today_data, "summary")[[Type = 1]]$duration[[1]]$`Dur (ms)` %>% unique %>% as.list()
   current_frequencies = pluck(today_data, "summary", 1, "Freq (kHz)") %>% unique %>% as.list()
   today_graph_data = today_data %>% unnest_wider(stats) %>% unnest(dprime) %>% unnest(reaction) %>% unnest(FA_detailed, names_sep = "_")
-
+  
   # minimize data to mess with
   # need table with date, hit%, Frequency, Duration, dB, rxn, dprime
   graph_data =
@@ -85,9 +86,9 @@ Generate_Graph <- function(rat_name, ratID) {
     unnest_wider(assignment) %>%
     filter(phase == current_phase & analysis_type == current_analysis_type) %>%
     filter(invalid != "TRUE")
-
+  
   #print(graph_data)
-
+  
   # Remove unnecessary trial data due to being post-baseline
   if(is_post_baseline){
     cut_off_date = filter(rat_archive, Rat_ID == ratID)$HL_date
@@ -99,9 +100,9 @@ Generate_Graph <- function(rat_name, ratID) {
       filter(analysis_type == current_analysis_type)
     if(nrow(filtered_graph_data) > 1) graph_data = filtered_graph_data
   }
-
+  
   #print(graph_data)
-
+  
   graph_data =
     graph_data %>%
     # need to do rowwise for the pluck
@@ -115,8 +116,8 @@ Generate_Graph <- function(rat_name, ratID) {
       else
         .
     }
-
-
+  
+  
   #TODO confirm time save
   # graph_data =
   #   graph_data %>%
@@ -124,16 +125,16 @@ Generate_Graph <- function(rat_name, ratID) {
   #   mutate(threshold = map_vec(stats, ~ .['threshold']),
   #          dprime = map_vec(stats, ~ .['dprime']),
   #          reaction = map_vec(stats, ~ .['reaction']))
-
+  
   graph_data =
-  graph_data %>%
-  unnest_wider(stats)
-
-# Graph production --------------------------------------------------------
+    graph_data %>%
+    unnest_wider(stats)
+  
+  # Graph production --------------------------------------------------------
   # takes graph_data data frame (which has nested dfs) and produces 3 graphs -
   # one for dprime, one for reaction and one for hit%
-
-
+  
+  
   # Gap Detection -----------------------------------------------------------
   if (str_detect(current_analysis_type, pattern = "Gap")) {
     # Calculate TH
@@ -144,8 +145,8 @@ Generate_Graph <- function(rat_name, ratID) {
       if(length(current_frequencies) == 1) TH = unnest(graph_data, threshold)$TH %>% mean(na.rm = TRUE)
       else stop("More than one frequency on a Gap file. This requires handling in the graphing code.")
     }
-
-    # dprime graph ##########
+    
+    ## dprime graph ##########
     what_to_graph = "dprime"
     x_column = "Dur"; y_column = "dprime"
     if (current_analysis_type == "Training - Gap") dprime_graph = Blank_Grapher(ggplot(graph_data))
@@ -158,7 +159,7 @@ Generate_Graph <- function(rat_name, ratID) {
     }
     # Add axis labels
     dprime_graph = dprime_graph + labs(x = "Gap Duration", y = "d'")
-
+    
     # Reaction graph ##########
     what_to_graph = "reaction"
     x_column = "Dur (ms)"; y_column = "Rxn"
@@ -166,29 +167,29 @@ Generate_Graph <- function(rat_name, ratID) {
     rxn_graph = graph_data %>%
       unnest(what_to_graph) %>%
       ggplot(aes(x = `Dur (ms)`, y = Rxn))
-
+    
     if (current_analysis_type == "Training - Gap") rxn_graph = Range_Grapher(rxn_graph)
-      # you can remove non-relevant data by filtering rxn_graph$data but this
-      # seems unnecessary and bad for day 1 of new stim
+    # you can remove non-relevant data by filtering rxn_graph$data but this
+    # seems unnecessary and bad for day 1 of new stim
     else rxn_graph = Line_Grapher(rxn_graph)
     # Add axis labels
     rxn_graph = rxn_graph + labs(x = "Gap Duration", y = "Reaction Time")
-
+    
     # Hit % graph  ##########
     # Need to add hit_detailed to do this
-
+    
   }
-
-
+  
+  
   # BBN ---------------------------------------------------------------------
   if (str_detect(current_analysis_type, pattern = "BBN")) {
     # Check for multiple durations in today's data
     has_multiple_durations = length(current_durations) > 1
-
+    
     if(has_multiple_durations){
       # Set today's data to 50 ms so only one of the multiple durations is graphed
       today_graph_data =  today_graph_data %>% filter(Dur == 50 & `Dur (ms)` == 50)
-
+      
       # Select only Mixed duration files for comparison, unless today is the 1st
       # day of Mixed durations (i.e. there is only 1 row which is today)
       filtered_graph_data = filter(graph_data, detail == current_detail)
@@ -197,10 +198,10 @@ Generate_Graph <- function(rat_name, ratID) {
       # Select only current duration files for comparison, unless today is the
       # 1st day of current duration (i.e. there is only 1 row which is today)
       filtered_graph_data = filter(graph_data, str_detect(file_name,
-                                                 pattern = glue("_{current_durations}ms_")))
+                                                          pattern = glue("_{current_durations}ms_")))
       if(nrow(filtered_graph_data) > 1) graph_data = filtered_graph_data
     }
-
+    
     # Calculate TH
     # There is no TH possible in Training files
     # In the case of multiple durations, we take 50ms as this is the most restrictive
@@ -211,9 +212,9 @@ Generate_Graph <- function(rat_name, ratID) {
           .$temp %>% mean(na.rm = TRUE)
       } else unnest(graph_data, threshold)$TH %>% mean(na.rm = TRUE)
     }
-
-
-    # dprime graph ##########
+    
+    
+    ## dprime graph ##########
     what_to_graph = "dprime"
     x_column = "dB"; y_column = "dprime"
     if (current_analysis_type == "Training - BBN") dprime_graph = Blank_Grapher(ggplot(graph_data))
@@ -226,7 +227,7 @@ Generate_Graph <- function(rat_name, ratID) {
     }
     # Add axis labels
     dprime_graph = dprime_graph + labs(x = "Intensity (dB)", y = "d'")
-
+    
     # Reaction graph ##########
     what_to_graph = "reaction"
     x_column = "Inten (dB)"; y_column = "Rxn"
@@ -234,31 +235,31 @@ Generate_Graph <- function(rat_name, ratID) {
     rxn_graph = graph_data %>%
       unnest(all_of(what_to_graph)) %>%
       ggplot(aes(x = `Inten (dB)`, y = Rxn))
-
+    
     if (current_analysis_type == "Training - BBN") rxn_graph = Range_Grapher(rxn_graph)
     # you can remove non-relevant data by filtering rxn_graph$data but this
     # seems unnecessary and bad for day 1 of new stim
     else rxn_graph = Line_Grapher(rxn_graph)
     # Add axis labels
     rxn_graph = rxn_graph + labs(x = "Intensity (dB)", y = "Reaction Time")
-
-
+    
+    
     # Hit % graph  ##########
     # Need to add hit_detailed to do this
-
+    
   }
-
-
+  
+  
   # Tones ---------------------------------------------------------------------
   if (str_detect(current_analysis_type, pattern = "Tone")) {
     # Check for multiple durations in today's data
     has_multiple_frequencies = length(current_frequencies) > 1
-
+    
     if(has_multiple_frequencies){
       # Set today's data to 8kHz so only one of the multiple frequencies is graphed
       today_graph_data =  today_graph_data %>% filter(Freq == 8 & `Freq (kHz)` == 8)
     }
-
+    
     # Calculate TH
     # There is no TH possible in Training files
     # In the case of multiple durations, we take 50ms as this is the most restrictive
@@ -269,9 +270,9 @@ Generate_Graph <- function(rat_name, ratID) {
           .$temp %>% mean(na.rm = TRUE)
       } else unnest(graph_data, threshold)$TH %>% mean(na.rm = TRUE)
     }
-
-
-    # dprime graph ##########
+    
+    
+    ## dprime graph ##########
     what_to_graph = "dprime"
     x_column = "dB"; y_column = "dprime"
     if (current_analysis_type == "Training - Tone") dprime_graph = Blank_Grapher(ggplot(graph_data))
@@ -284,7 +285,7 @@ Generate_Graph <- function(rat_name, ratID) {
     }
     # Add axis labels
     dprime_graph = dprime_graph + labs(x = "Intensity (dB)", y = "d'")
-
+    
     # Reaction graph ##########
     what_to_graph = "reaction"
     x_column = "Inten (dB)"; y_column = "Rxn"
@@ -292,46 +293,46 @@ Generate_Graph <- function(rat_name, ratID) {
     rxn_graph = graph_data %>%
       unnest(what_to_graph) %>%
       ggplot(aes(x = `Inten (dB)`, y = Rxn))
-
+    
     if (current_analysis_type == "Training - Tone") rxn_graph = Range_Grapher(rxn_graph)
     # you can remove non-relevant data by filtering rxn_graph$data but this
     # seems unnecessary and bad for day 1 of new stim
     else rxn_graph = Line_Grapher(rxn_graph)
     # Add axis labels
     rxn_graph = rxn_graph + labs(x = "Intensity (dB)", y = "Reaction Time")
-
-
+    
+    
     # Hit % graph  ##########
     # Need to add hit_detailed to do this
-
+    
   }
-
+  
   # Octave ---------------------------------------------------------------------
   # TODO: untested for discrimination, note this will use Detailed FA I believe
   if (str_detect(current_analysis_type, pattern = "Octave")) {
     # filter to only the same detail (i.e. Normal or reverse) as we don't need the other
     graph_data = filter(graph_data, detail == current_detail)
-
+    
     # If octave discrimination (i.e. has steps), convert to octave steps rather than raw frequency
     has_multiple_frequencies = length(current_frequencies) > 2
-
+    
     if(has_multiple_frequencies){
       # only go is consistent as the number of no-gos fluctuates
       go_kHz = filter(today_graph_data$summary[[1]], Type == 1)$`Freq (kHz)`
-
+      
       # Add octave columns: octave_fraction is the fraction of the octave,
       # octave_step is the nearest 16th step (i.e. x/16)
       graph_data =
         graph_data %>%
         mutate(FA_detailed = map(FA_detailed, ~ mutate(., octave_fraction = log(go_kHz/`Freq (kHz)`)/log(2),
-                                                          octave_step = abs(round(octave_fraction * 16)))))
+                                                       octave_step = abs(round(octave_fraction * 16)))))
     }
-
+    
     # Calculate TH
     # None, but could consider calculating where more like go than no-go i.e. > 50% FA%
-
-
-    # dprime graph ##########
+    
+    
+    ## dprime graph ##########
     what_to_graph = "dprime"
     x_column = "FA_detailed_Freq (kHz)"; y_column = "FA_detailed_dprime"
     if (current_analysis_type == "Training - Octave") dprime_graph = Blank_Grapher(ggplot(graph_data))
@@ -344,7 +345,7 @@ Generate_Graph <- function(rat_name, ratID) {
     }
     # Add axis labels
     dprime_graph = dprime_graph + labs(x = "Frequency (kHz)", y = "d'")
-
+    
     # Reaction graph ##########
     what_to_graph = "reaction"
     x_column = "Inten (dB)"; y_column = "Rxn"
@@ -355,15 +356,15 @@ Generate_Graph <- function(rat_name, ratID) {
       Range_Grapher
     # Add axis labels
     rxn_graph = rxn_graph + labs(x = "Intensity (dB)", y = "Reaction Time")
-
-
+    
+    
     # FA % graph  ##########
     what_to_graph = "False Alarm"
     x_column = "FA_detailed_Freq (kHz)"; y_column = "FA_detailed_FA_percent_detailed"
     if (current_analysis_type == "Training - Octave") hit_graph = Blank_Grapher(ggplot(graph_data))
     else {
       today_graph_data = mutate(today_graph_data, FA_detailed_FA_percent_detailed = FA_detailed_FA_percent_detailed * 100)
-
+      
       # Graph
       hit_graph = graph_data %>%
         unnest(FA_detailed, names_sep = "_") %>%
@@ -373,23 +374,26 @@ Generate_Graph <- function(rat_name, ratID) {
     }
     # Add axis labels
     hit_graph = hit_graph + labs(x = "Frequency (kHz)", y = "False Alarm %")
-
+    
   }
-
+  
   # Oddball ---------------------------------------------------------------------
   # TODO: untested for discrimination, note this will use Detailed FA I believe
   if (str_detect(current_analysis_type, pattern = "Oddball")) {
     # Calculate TH
     # None
-
-    # dprime graph ##########
+    
+    ## dprime graph ##########
     # Does not exist but could do detailed FA graph instead
     what_to_graph = "dprime"
     x_column = "dB"; y_column = "dprime"
     dprime_graph = Blank_Grapher(ggplot(graph_data))
     # Add axis labels
-    dprime_graph = dprime_graph + labs(x = "Intensity (dB)", y = "d'")
-
+    dprime_graph = dprime_graph + 
+      scale_x_continuous(breaks = seq(from = 1, to = 10, by = 1))
+    labs(x = "Intensity (dB)", y = "d'")
+    
+    
     # Reaction graph ##########
     what_to_graph = "reaction"
     x_column = "Inten (dB)"; y_column = "Rxn"
@@ -397,22 +401,22 @@ Generate_Graph <- function(rat_name, ratID) {
     rxn_graph = graph_data %>%
       unnest(what_to_graph) %>%
       ggplot(aes(x = `Inten (dB)`, y = Rxn))
-
+    
     if (current_analysis_type == "Training - Oddball") rxn_graph = Range_Grapher(rxn_graph)
     # you can remove non-relevant data by filtering rxn_graph$data but this
     # seems unnecessary and bad for day 1 of new stim
     else rxn_graph = Line_Grapher(rxn_graph)
     # Add axis labels
     rxn_graph = rxn_graph + labs(x = "Intensity (dB)", y = "Reaction Time")
-
-
+    
+    
     # Hit % graph  ##########
     # Need to add hit_detailed to do this
-
+    
   }
-
+  
   # print(rxn_graph)
-
+  
   return(tibble_row(dprime_graph = dprime_graph, rxn_graph = rxn_graph))
 }
 
