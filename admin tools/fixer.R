962479843c850ca88382de00105f805610fb53be
Noelle James <github@nohelix.com>
2024-02-13 13:03:01 -0600

    Fixer clean up and added invalidation and phase modification

--- admin tools/fixer.R
+++ admin tools/fixer.R
@@ -1,17 +1,16 @@
-fixer <- function(tableRow, newValue_task, newValue_detail) {
-  
-  # print(newValue_task)
-  # print(newValue_detail)
+fixer <- function(tableRow, newValue_phase, newValue_task, newValue_detail, newValue_invalid) {
   
   df = run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment)
+    select(date, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment)
   
   # set value to change
+  if(!is.na(newValue_phase)) run_archive[tableRow,]$assignment[[1]]$phase = newValue_phase
   if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task = newValue_task
   if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail = newValue_detail
+  if(newValue_invalid != run_archive[tableRow,]$invalid) run_archive[tableRow,]$invalid = newValue_invalid
   
   df = bind_rows(df, run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment))
+                   select(date, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment))
   
   print(df)
   
@@ -20,8 +19,10 @@ fixer <- function(tableRow, newValue_task, newValue_detail) {
               graphics = TRUE),
          # 1 (Yes): Write file
          {# Push modification -------------------------------------------------------
+           if(!is.na(newValue_phase)) run_archive[tableRow,]$assignment[[1]]$phase <<- newValue_phase
            if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task <<- newValue_task
            if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail <<- newValue_detail
+           if(newValue_invalid != run_archive[tableRow,]$invalid) run_archive[tableRow,]$invalid <<- newValue_invalid
            outcome = "Fixed"},
          # 2 (No): Abort
          {outcome = "No change"})
@@ -35,17 +36,19 @@ fixer <- function(tableRow, newValue_task, newValue_detail) {
 InitializeMain()
 
 # filter to rowid
-run_archive %>% rowid_to_column %>% 
-  filter(rat_name %in% c("Bronze3", "Bronze4")) %>%
+run_archive %>% rowid_to_column %>%
+  filter(rat_name %in% c("Bronze3")) %>%
   unnest_wider(assignment) %>%
+  # filter(phase == "Gap Detection") %>%
+  # filter(str_detect(assigned_file_name, pattern = "(25-85|15-75|10-70|20-50|25-55|15-45)")) %>%
   arrange(desc(date)) %>%
-  filter(date %in% c(20240113, 20240112, 20240111, 20240110)) %>%
+  filter(date == 20240213) %>%
   # mutate(BG = str_extract(file_name, pattern = "(?<=BG_PKN_).*(?=dB)")) %>% 
   # filter(task == "Reset")
-
-# then lapply
+  
+  # then lapply
   .$rowid %>%
-  lapply(fixer, newValue_task = NA, newValue_detail = "Alone")
+  lapply(fixer, newValue_phase = NA, newValue_task = "Rxn", newValue_detail = NA, newValue_invalid = "")
 
 # Save modification -------------------------------------------------------
 save(run_archive, file = paste0(projects_folder, "run_archive.Rdata"), ascii = TRUE, compress = FALSE)
