efd7d3cfb8bd62d02d6cef3b9f8bffdd79fcbc92
Noelle James <github@nohelix.com>
2024-02-16 09:06:58 -0600

    Fixed invalidation to work in Fixer.
    Bug of changing for diplay purposes prevented actual global changing. Now runs on a flag.

--- admin tools/fixer.R
+++ admin tools/fixer.R
@@ -1,16 +1,18 @@
 fixer <- function(tableRow, newValue_phase, newValue_task, newValue_detail, newValue_invalid) {
   
   df = run_archive[tableRow,] %>% 
-    select(date, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment)
+    select(date, time, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment)
   
   # set value to change
   if(!is.na(newValue_phase)) run_archive[tableRow,]$assignment[[1]]$phase = newValue_phase
   if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task = newValue_task
   if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail = newValue_detail
-  if(newValue_invalid != run_archive[tableRow,]$invalid) run_archive[tableRow,]$invalid = newValue_invalid
+  if(newValue_invalid != run_archive[tableRow,]$invalid) {
+    run_archive[tableRow,]$invalid = newValue_invalid
+    change_invalid = TRUE} else {change_invalid = FALSE}
   
   df = bind_rows(df, run_archive[tableRow,] %>% 
-                   select(date, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment))
+                   select(date, time, rat_name, rat_ID, assignment, invalid) %>% unnest_wider(assignment))
   
   print(df)
   
@@ -22,27 +24,21 @@ fixer <- function(tableRow, newValue_phase, newValue_task, newValue_detail, newV
            if(!is.na(newValue_phase)) run_archive[tableRow,]$assignment[[1]]$phase <<- newValue_phase
            if(!is.na(newValue_task)) run_archive[tableRow,]$assignment[[1]]$task <<- newValue_task
            if(!is.na(newValue_detail)) run_archive[tableRow,]$assignment[[1]]$detail <<- newValue_detail
-           if(newValue_invalid != run_archive[tableRow,]$invalid) run_archive[tableRow,]$invalid <<- newValue_invalid
+           if(change_invalid) run_archive[tableRow,]$invalid <<- newValue_invalid
            outcome = "Fixed"},
          # 2 (No): Abort
          {outcome = "No change"})
-  
-  # InitializeMain()
-  # run_archive[tableRow,] %>% 
-  #   select(date, rat_name, rat_ID, assignment) %>% unnest_wider(assignment) %>% select(-comment) %>% print
-  
-  return(outcome)
 }
 InitializeMain()
 
 # filter to rowid
 run_archive %>% rowid_to_column %>%
-  filter(rat_name %in% c("Bronze3")) %>%
+  filter(rat_name %in% c("Green23")) %>%
   unnest_wider(assignment) %>%
   # filter(phase == "Gap Detection") %>%
   # filter(str_detect(assigned_file_name, pattern = "(25-85|15-75|10-70|20-50|25-55|15-45)")) %>%
   arrange(desc(date)) %>%
-  filter(date == 20240213) %>%
+  filter(date == 20240215) %>%
   # mutate(BG = str_extract(file_name, pattern = "(?<=BG_PKN_).*(?=dB)")) %>% 
   # filter(task == "Reset")
   
