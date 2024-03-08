76947a3acc72ebb4fae5b5e5af9dd175a0b0b44d
Noelle James <github@nohelix.com>
2024-03-08 10:42:57 -0600

    Prevents hard crash due to hacky solution mentioned in #99

--- main.R
+++ main.R
@@ -803,7 +803,7 @@ Process_File <- function(file_to_load, name, weight, observations, exclude_trial
             else {
               computed_file_name = paste0(computed_file_name, delay, "s_", catch_number, "catch_", lockout, "s")
               assigned_file = filter(rat_archive, Rat_ID == run_properties$rat_ID)$Assigned_Filename
-              expected_delay <<- str_extract(assigned_file, "(?<=dB_)[:digit:]+?.*[:digit:]+?(?=s_)") %>% str_replace("-", " ")
+              if(assigned_file != "") expected_delay <<- str_extract(assigned_file, "(?<=dB_)[:digit:]+?.*[:digit:]+?(?=s_)") %>% str_replace("-", " ")
             }
             # delay_in_filename <<- FALSE
 
