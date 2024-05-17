762dc81f57be2d28ea0fb1fbb082c066a7fd97aa
Noelle James <github@nohelix.com>
2024-05-17 08:58:51 -0500

    Added Fmr1 SD back archive

--- admin tools/import_old_excel.R
+++ admin tools/import_old_excel.R
@@ -14,6 +14,8 @@ readExcel <- function(filename, cols = c(1:3, 23, 28:32)) {
                dplyr::mutate(rat_name = stringr::str_replace_all(string = X, pattern = "[:space:]", replacement = ""),
                              Invalid = as.character(Invalid))
   ) %>% dplyr::bind_rows() %>% dplyr::rename(Weight = X3) %>% setNames(names)
+  
+  print(as_tibble(r))
 
   return(r)
 }
@@ -21,14 +23,18 @@ readExcel <- function(filename, cols = c(1:3, 23, 28:32)) {
 writeLines("Creating old archives")
 cat("\tAdding: ")
 old_excel_archive = setNames(data.frame(matrix(ncol = 10, nrow = 0)), names)
-  cat("TTS...")
+cat("TTS...")
 old_excel_archive = rbind(old_excel_archive, readExcel("TTS_Gp1_Green-Orange.xlsx"))
 cat("Fmr1-LE...")
 old_excel_archive = rbind(old_excel_archive, readExcel("Fmr1-LE_Grp1_BP_LP_Purple.xlsx"))
+cat("Fmr1 SD...")
+old_excel_archive = rbind(old_excel_archive, readExcel("Fmr1_SD_Gp2_RedTealLime.xlsx", cols = c(1:3, 23, 27:32)))
 cat("Tsc2-LE...")
 old_excel_archive = rbind(old_excel_archive, readExcel("Tsc2_Eker_Gp1_RP_GP_TP.xlsx"))
-cat("Octave (pilot)...") 
-old_excel_archive = rbind(old_excel_archive, readExcel("Fmr1_SD_Gp1_Purple - Tone Discrimination.xlsx"))
+cat("Octave (Fmr1-LE pilot)...") 
+old_excel_archive = rbind(old_excel_archive, readExcel("Fmr1_SD_Gp1_Purple - Tone Discrimination.xlsx", cols = c(1:3, 23, 28:33)))
+cat("Octave (Fmr1 SD pilot)...") 
+old_excel_archive = rbind(old_excel_archive, readExcel("Fmr1_SD_Gp2_RedTealLime - Tone Discrimination.xlsx", cols = c(1:3, 24, 28:33)))
 #   cat("Gap Detection...")  
 # old_excel_archive = rbind(old_excel_archive, readExcel("GD_Grp1_Red_Teal.xlsx"))
   cat("Oddball (LE pilot)")  
